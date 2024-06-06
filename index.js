acb2953cb5e212c4544321700359f07180e40f21
ForystekVictoria <vicky.forystek@gmail.com>
2024-06-06 14:19:48 -0300

    "first commit"

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import routes from './src/routes/calificaciones.routes.js'
+
+
+const app = express()
+const PORT = 8080 
+
+app.use(express.json()) 
+app.use(express.urlencoded({extended: true}))
+
+app.use("/", routes);
+
+app.listen(PORT, () => console.log(`Server listening on: http://localhost:8080`));
+
+app.on("Error", (error)=> console.log(`Server error: ${error}`))
+
+app.get("/",(req,res)=>{
+    res.send("HOLA!")
+});
